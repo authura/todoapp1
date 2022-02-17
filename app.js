e3527c6b2c0d6ec410a475f8b7de684f93a6acbd
authura <rhoferdevs@gmail.com>
2022-02-17 11:29:39 -0500

    recreated pseudocode in js for practice

--- app.js
+++ app.js
@@ -1,50 +1,66 @@
+// event happens when page loads
 window.addEventListener("load", () => {
 
+    // get form input, and tasks DOM elements
     const form = document.querySelector("#new-task-form");
 
     const input = document.querySelector("#new-task-input");
 
     const listElement = document.querySelector("#tasks");
 
+    // event happens when form is submitted
     form.addEventListener("submit", (e) => {
 
+        // prevent page refresh(default submit action)
         e.preventDefault();
 
         const task = input.value;
 
+        // if there is no value in the input
         if (!task) {
             alert("Please fill out field.");
             return;
         }
 
-        const taskElement = document.createElement("div");
+        // Create divs and give them class elements
+
+        const taskElement = document.createElement('div');
+
+        // add new div to class list "task" 
         taskElement.classList.add("task");
 
-        const taskContentElement = document.createElement("div");
+        const taskContentElement = document.createElement('div');
         taskContentElement.classList.add("content");
 
+        // append child elements to parents
         taskElement.appendChild(taskContentElement);
 
         const taskInputElement = document.createElement("input");
         taskInputElement.classList.add("text");
+
+        //set input type to text
         taskInputElement.type = "text";
 
         taskContentElement.appendChild(taskInputElement);
 
+        // what outputs to the task list is the value associated with the form
         taskInputElement.value = task;
 
+        //set input to readonly
         taskInputElement.setAttribute("readonly", "readonly");
 
+        //create actions div
         const taskActionsElement = document.createElement("div");
         taskActionsElement.classList.add("actions");
 
+        // create edit and del buttons
         const taskEditElement = document.createElement("button");
         taskEditElement.classList.add("edit");
         taskEditElement.innerHTML = "Edit"
 
         const taskDeleteElement = document.createElement("button");
         taskDeleteElement.classList.add("delete");
-        taskDeleteElement.innerHTML = "Del";
+        taskDeleteElement.innerHTML = "Del"
 
         taskActionsElement.appendChild(taskEditElement);
 
@@ -55,6 +71,7 @@ window.addEventListener("load", () => {
         listElement.appendChild(taskElement);
         form.reset();
 
+        // click event for edit button
         taskEditElement.addEventListener("click", () => {
             if (taskEditElement.innerHTML.toLowerCase() == "edit") {
                 taskInputElement.removeAttribute("readonly");
@@ -65,6 +82,7 @@ window.addEventListener("load", () => {
             }
         });
 
+        // click event for delete button
         taskDeleteElement.addEventListener("click", () => {
             listElement.removeChild(taskElement);
         });
